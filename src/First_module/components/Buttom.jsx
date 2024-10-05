45801b7fd27d636573dc4da1fc00a4080f34fd62
JuanHono492 <jhono@autonoma.edu.pe>
2024-10-05 02:39:37 -0500

    V. 0.1

---
+++ src/First_module/components/Buttom.jsx
@@ -0,0 +1,9 @@
+import './Buttom.css';
+import { Button } from '@mui/material';
+function Buttom() {
+  return (
+        <Button variant="outlined">Outlined</Button>
+  );
+}
+
+export default Buttom;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Inicio from './First_module/views/Inicio';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Inicio/>
   </React.StrictMode>
 );
 
